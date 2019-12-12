0cde55440cdb2f96420ff5b7024a8c9e2a977ba1
Yevhen Konyk <yevhen.konyk@gmail.com>
2019-12-12 15:22:18 +0200

    Added Profile component placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Profile from "./Profile/Profile";
+import User from "./Profile/user.json";
 
 const App = () => (
   // React Fragment - так как для рендеринга нескольких элементов нужен общий родитель.
-  <></>
+  <>
+    <Profile user={User} />
+  </>
 );
 
 export default App;
